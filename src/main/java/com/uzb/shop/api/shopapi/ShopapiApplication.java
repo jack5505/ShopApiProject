bc12f055366a0aff17c0b478d4d89ffe35a4e16c
Sabirov Jakhongir <jahon.080879@gmail.com>
2021-04-03 16:37:27 +0500

    Init Project

---
+++ src/main/java/com/uzb/shop/api/shopapi/ShopapiApplication.java
@@ -0,0 +1,13 @@
+package com.uzb.shop.api.shopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopapiApplication.class, args);
+    }
+
+}
